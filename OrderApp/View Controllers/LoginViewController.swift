b63483d0935c1b0a40641105320e0a3dbfc63fcc
Lucas Dahl <LucasDahl425@yahoo.com>
2020-06-26 11:20:40 -0700

    Initial Commit

---
+++ OrderApp/View Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  OrderApp
+//
+//  Created by Lucas Dahl on 6/26/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
